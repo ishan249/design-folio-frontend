a35e6c130e7887cdf055bebf456417cd2ba4c555
ishan249 <249patelishan@gmail.com>
2023-03-06 19:10:12 +0530

    link changes

--- src/components/UserCreated/Usercreated.jsx
+++ src/components/UserCreated/Usercreated.jsx
@@ -14,9 +14,9 @@ function Usercreated() {
           <p className="px-4 py-2 text-md">
             Your personal portfolio is ready. Here is link, check it out{" "}
           </p>
-          <Link to ={`designfolio.onrender.com/${username}`}>
+          <a target="_blank" href={`designfolio.onrender.com/${username}`}>
           <div className="text-center user-link">designfolio.onrender.com/{username}</div>
-          </Link>
+          </a>
         </div>
       </div>
     </div>
